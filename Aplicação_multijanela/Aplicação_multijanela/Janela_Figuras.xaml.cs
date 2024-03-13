2589dc24106a0841923ab5f866535c83218ef4e2
LeonorTPinto <160856234+LeonorTPinto@users.noreply.github.com>
2024-03-13 11:34:32 +0000

    continuação

---
+++ Aplicação_multijanela/Aplicação_multijanela/Janela_Figuras.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplicação_multijanela
+{
+    /// <summary>
+    /// Lógica interna para Janela_Figuras.xaml
+    /// </summary>
+    public partial class Janela_Figuras : Window
+    {
+        public Janela_Figuras()
+        {
+            InitializeComponent();
+        }
+    }
+}
